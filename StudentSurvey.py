90d640747cd78815d5a952c4eb01079066870481
adawiyahh <adawiyah2k06@gmail.com>
2025-10-19 14:44:11 +0800

    Update StudentSurvey.py

--- StudentSurvey.py
+++ StudentSurvey.py
@@ -13,6 +13,12 @@ st.subheader("Dataset: Survey from Law Faculty")
 # Load dataset from GitHub 
 # Replace this URL with your actual raw CSV file link
 csv_url = "https://raw.githubusercontent.com/adawiyahh/sciviz2025/refs/heads/main/law_faculty_survey.csv"
+col1, col2, col3, col4 = st.columns(4)
+    
+    col1.metric(label="PLO 2", value=f"3.3", help="PLO 2: Cognitive Skill", border=True)
+    col2.metric(label="PLO 3", value=f"3.5", help="PLO 3: Digital Skill", border=True)
+    col3.metric(label="PLO 4", value=f"4.0", help="PLO 4: Interpersonal Skill", border=True)
+    col4.metric(label="PLO 5", value=f"4.3", help="PLO 5: Communication Skill", border=True)
 
 # Read dataset directly from GitHub
 law_df = pd.read_csv(csv_url)
